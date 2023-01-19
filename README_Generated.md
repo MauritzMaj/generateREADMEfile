196af9f93ba5d36939f81ed785f60a9a632ed15c
MauritzMaj <mauritz.majelyne@hotmail.com>
2023-01-19 10:41:49 -0500

    final fixes

---
+++ README_Generated.md
@@ -0,0 +1,47 @@
+
+# Title [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+
+## table of contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [License](#license)
+* [Questions](#questions)
+
+
+## <a {name="description"></a>Description
+
+Short Description
+
+## <a {name="installation"></a>Installation
+
+No installation required
+
+## <a {name="usage"></a>Usage
+
+Use appropriately
+
+
+## <a {name="contributing"></a>Contributing
+
+Contribute as you see fit
+
+
+## <a {name="tests"></a>Tests
+
+N/A
+
+## <a {name="license"></a>License 
+
+This project is covered under the following license: MIT License
+
+## <a {name="questions"></a>Questions
+
+Created by user: MauritzMaj
+
+https://github.com/MauritzMaj
+
+mm@noemail.com
--- index.js
+++ index.js
@@ -4,75 +4,62 @@ const badges = [
 
   {
   name:"Apache License 2.0",
-  id:1,
   link:"[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
   },
 
   {
   name:"GNU General Public License v3.0",
-  id:2,
   link:"[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
   },
 
   {
   name:"MIT License",
-  id:3,
   link:"[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
   },
 
   {
   name:'BSD 2-Clause "Simplified" License',
-  id:4,
   link:"[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)",
   },
 
   {
   name:'BSD 3-Clause "New" or "Revised" License',
-  id:5,
   link:"[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)",
   },
 
   {
   name:"Boost Software License 1.0",
-  id:6,
   link:"[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)",
   },
 
   {
   name:"Creative Commons Zero v1.0 Universal",
-  id:7,
   link:"[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)",
   },
 
   {
   name:"Eclipse Public License 2.0",
-  id:8,
   link:"[![License](https://img.shields.io/badge/License-EPL_2.0-red.svg)](https://opensource.org/licenses/EPL-2.0)",
   },
 
   {
   name:"GNU Affero General Public License v3.0",
-  id:9,
   link:"[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)",
   },
 
   {name:"GNU General Public License v2.0",
-  id:10,
   link:"[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)",
   },
 
   {name:"GNU Lesser General Public License v2.1",
-  id:11,
   link:"[![License: LGPL v2](https://img.shields.io/badge/License-LGPL_v2-blue.svg)](https://www.gnu.org/licenses/lgpl-2.0)",
   },
 
   {name:"Mozilla Public License 2.0",
-  id:12,
   link:"[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)",
   },
 
   {name:"The Unlicense",
-  id:13,
   link:"[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)",
   },
 
@@ -83,14 +70,14 @@ function Renderlicensebadge(License)  {
 
   for (let i=0; i<badges.length; i++) {
 
-      if (License === badges[i].name) {
+      if (License === badges[i].name)
+       {
       
     return badges[i].link;
    
-      } else { return ''};
-
-  
-  }};  
+    }}
+    return ''
+  };  
 
 function Renderlicenseinfo(License) {
   console.log(License);
@@ -99,14 +86,12 @@ function Renderlicenseinfo(License) {
 for (let i=0; i <badges.length; i++) {
 
   if (License == badges[i].name) {
-
-    console.log(badges[i].name);
     
-  return "This project is covered under the" + License;
+  return "This project is covered under the following license: " + License;
+}}
 
-   } else { return 'This project has no license';}
+ return 'This project has no license';
 
-}
 };
 
 
